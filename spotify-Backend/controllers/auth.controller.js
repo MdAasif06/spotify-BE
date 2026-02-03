0c2fc17349e53c2403c3a6afffc76c6002f99fb8
MdAasif06 <abdurm477@gmail.com>
2026-02-03 13:19:15 +0530

    register function created with cookie and validation

---
+++ spotify-Backend/controllers/auth.controller.js
@@ -0,0 +1,65 @@
+const userModel = require("../models/user.model.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password, role = "user" } = req.body;
+
+    // basic validation
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+    // check if user already exists
+    isUserExists = await userModel.findOne({
+      $or: [{ username }, { email }],
+    });
+    if (isUserExists) {
+      return res.status(409).json({
+        message: "User already exits",
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = await userModel.create({
+      username,
+      email,
+      password: hashPassword,
+      role,
+    });
+    // generate token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" },
+    );
+    // set cookie securely
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.status(201).json({
+      message: "user register successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error("Register User Error:", error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { registerUser };
--- spotify-Backend/src/app.js
+++ spotify-Backend/src/app.js
@@ -1,7 +1,10 @@
 const express=require("express")
 const cookirParser=require("cookie-parser")
+const authRoute=require("../routes/auth.route.js")
 const app=express()
 
 app.use(express.json())
+app.use(cookirParser())
+app.use('/api/auth',authRoute)
 
 module.exports=app
